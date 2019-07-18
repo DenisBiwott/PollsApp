25a89ddfaa641eb0be6448c9970c2d88af5c3ff3
Denis Biwott <denisbiwott@healthcloud.co.ke>
2019-07-18 17:05:13 +0300

    Make changes in mysite/urls.py - This makes sure the Polls is in the root url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 # def trigger_error(request):
 #   division_by_zero = 1 / 0
 
 urlpatterns = [
+    url(r'^', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
     path('', include('django_prometheus.urls')),
 #    path('sentry-debug/', trigger_error),
 ]
