859d67e01b2db4276da3e6c0cee3958fa9d0ef82
Denis Biwott <denisbiwott@healthcloud.co.ke>
2019-07-24 09:45:36 +0300

    Changin package directory to PollsPackage

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords='',
-    packages=['PollsApp'],
+    packages=['PollPackage'],
     install_requires=[
         'requests==2.20.0',
     ],
