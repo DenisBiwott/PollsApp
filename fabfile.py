68a89c90b1f10c08c545577dfbf42274d6d77235
Denis Biwott <denisbiwott@healthcloud.co.ke>
2019-07-31 10:46:26 +0300

    Verison 0,0,7

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-from fabric.api import local
-
-
-def cov_pwd():
-    local("python3 manage.py runserver")
-    #local("coverage run --source='.' manage.py test")
-    #local("coverage report")
-    local("pwd")
