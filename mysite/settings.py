4a070cf393edb47c8926f3a3847b636381d0d5f4
Denis Biwott <denisbiwott@healthcloud.co.ke>
2019-08-04 23:06:36 +0300

    Hide variables and turn debug to false for security

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(_w*1do61sfrkna#xr3084+ec-m7zp$cxv!fleu0(1kte!9tx8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
